9e8592eff9da9944f43c2a070898abf8368e2981
ITE-DCACERES\dcaceres <asesino5>
2017-05-17 13:50:31 +0200

    no message

--- app/cotxes/client/client.component.ts
+++ app/cotxes/client/client.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
   styleUrls: ['./client.component.css']
 })
+
 export class ClientComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
